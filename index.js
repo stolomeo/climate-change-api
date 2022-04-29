4252395b1f6bc64da688ebe236b5fdaf9a25b9df
stolomeo <tolomeosebastian@gmail.com>
2022-04-28 23:27:20 -0400

    Get html from source

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ import cheerio from "cheerio";
 const PORT = 8080;
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Welcome to my Climage Change API");
+});
+
+app.get("/news", (req, res) => {
+  axios
+    .get("https://www.theguardian.com/environment/climate-crisis")
+    .then((response) => {
+      const html = response.data;
+      console.log(html);
+    });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
